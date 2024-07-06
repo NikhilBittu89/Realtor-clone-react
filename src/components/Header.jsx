5b663087cddab8eb7c6da5ff71bc292210571fc1
ravitvavilala <rvavilala@albany.edu>
2024-07-05 20:12:15 -0400

    create pages and routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <Header>
+        <div>
+            <img className='h-5 cursor-pointer' src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" alt="realtor" />
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>offers</li>
+                <li>Sign in</li>
+               
+            </ul>
+        </div>
+      </Header>
+    </div>
+  )
+}
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Offers() {
+  return (
+    <div>
+      <h1>offers</h1>
+    </div>
+  )
+}
