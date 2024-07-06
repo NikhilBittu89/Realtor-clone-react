b4add5bb506527375f89539bd28f111fa0582ac3
ravitvavilala <rvavilala@albany.edu>
2024-07-05 21:20:04 -0400

    create header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 
 
- // Or wherever you placed your Tailwind CSS file
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+ // Or wherever you
+ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/profile" element={<Profile />} /> 
-        <Route path="/sig-in" element={<Signin />} /> 
-        <Route path="/sig-up" element={<Signup />} /> 
-        <Route path="/forgot-password" element={<ForgotPassword />} /> 
-        <Route path="/offers" element={<Offers />} /> 
-      </Routes>
-    </Router>
-    
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
-  
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useLocation,useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location.pathname);
+
+  
+
+  function pathMathRouter(route) {
+    return route === location.pathname;
+  }
+
   return (
-    <div>
-      <Header>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
-            <img className='h-5 cursor-pointer' src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" alt="realtor" />
+          <img
+            className='h-5 cursor-pointer'
+            src='https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg'
+            alt='realtor'
+            onClick={()=>navigate('/')}
+          />
         </div>
         <div>
-            <ul>
-                <li>Home</li>
-                <li>offers</li>
-                <li>Sign in</li>
-               
-            </ul>
+          <ul className='flex space-x-10'>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer${
+                pathMathRouter('/') && 'text-black border-b-red-500'
+              }`}
+              onClick={()=>navigate('/')}
+            >
+              Home
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
+                pathMathRouter('/offers') && 'text-black border-b-red-500'
+              }`}
+              onClick={()=>navigate('/offers')}
+            >
+              Offers
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent  cursor-pointer ${
+                pathMathRouter('/sign-in') && 'text-black border-b-red-500'
+              }`}
+              onClick={()=>navigate('/sign-in')}
+            >
+              Sign In
+            </li>
+          </ul>
         </div>
-      </Header>
+      </header>
     </div>
-  )
+  );
 }
